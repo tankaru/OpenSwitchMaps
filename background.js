e9e6aa7490e01dbaaa95da5d66e6937d16905b80
tankaru <muramototomoya@gmail.com>
2019-01-12 23:06:25 +0900

    メニュー表示を早く、したつもり

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ chrome.runtime.onInstalled.addListener(function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [
 			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'google.*maps'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'mapillary'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'https://maps.gsi.go.jp'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'https://www.openstreetcam.org/map'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'https://yandex.com/maps'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'https://demo.f4map.com/'},}),
-			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: 'www.openstreetmap.org'},})
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'www.mapillary.com'},}),
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'maps.gsi.go.jp'},}),
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'www.openstreetcam.org'},}),
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlMatches: '(yandex.com/maps)'},}),
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'demo.f4map.com'},}),
+			new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostEquals: 'www.openstreetmap.org'},})
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
