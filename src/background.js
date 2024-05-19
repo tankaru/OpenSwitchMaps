92e02f5454b45163557fbc8397445b9323221552
tankarup <muramototomoya@gmail.com>
2024-05-19 20:28:59 +0900

    if url is not http, ignore it.

--- src/background.js
+++ src/background.js
@@ -1,6 +1,13 @@
 const {isMatchingAMap} = require('./maps');
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	//https://github.com/tankaru/OpenSwitchMaps/issues/125
+	// if url is not http, skip
+	if (!tab.url.startsWith('http')){
+		chrome.pageAction.hide(tabId);
+		return;
+	}
+
   if (isMatchingAMap(tab.url)) {
     chrome.pageAction.show(tabId);
   } else {
